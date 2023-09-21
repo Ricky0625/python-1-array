86b9b1537c218bbf992cf38827bd30e78bc3f26c
Ricky0625 <rickywong0625@gmail.com>
2023-09-21 15:12:25 +0800

    done ex01

--- ex01/array2D.py
+++ ex01/array2D.py
@@ -10,11 +10,14 @@ def slice_me(family: list, start: int, end: int) -> list:
         if not isinstance(start, int) or not isinstance(end, int):
             raise AssertionError('start and end should be int')
 
+        # convert list into numpy array
         arr = np.array(family)
 
+        # check dimension of the array
         if arr.ndim != 2:
             raise AssertionError('must be a 2D array')
 
+        # will throw an error if the lists inside are not the same size
         new_arr = arr[start:end]
 
         print(f"My shape is : {arr.shape}")
